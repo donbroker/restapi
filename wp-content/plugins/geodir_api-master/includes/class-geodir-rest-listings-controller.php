eb739ca31a76448904285d6864e8ae52de5f32c9
qhong <frederickhong.5@gmail.com>
2016-09-20 18:20:47 -0600

    advert rest api

--- wp-content/plugins/geodir_api-master/includes/class-geodir-rest-listings-controller.php
+++ wp-content/plugins/geodir_api-master/includes/class-geodir-rest-listings-controller.php
@@ -67,18 +67,43 @@ class Geodir_REST_Listings_Controller extends WP_REST_Posts_Controller {
     public function get_collection_params() {
         $params = parent::get_collection_params();
                 
-        $sort_options = geodir_rest_get_listing_sorting( $this->post_type );
- 
-        $orderby            = array_keys( $sort_options['sorting'] );
-
-        $orderby_rendered   = $sort_options['sorting'];
-        $default_orderby    = $sort_options['default_sortby'];
-        $default_order      = $sort_options['default_sort'];
+        // $sort_options = geodir_rest_get_listing_sorting( $this->post_type );
+        // var_dump($sort_options); die();
+        // $orderby            = array_keys( $sort_options['sorting'] );
+        // $orderby_rendered   = $sort_options['sorting'];
+        // $default_orderby    = $sort_options['default_sortby'];
+        // $default_order      = $sort_options['default_sort'];
 
         $default_dist       = 250000;
         $default_lat        = 51.0530588;
         $default_lon        = -114.0625613;
-        $default_sort_by    = 'nearest';           
+        $default_sort_by    = 'nearest';
+        $sort_by_options    = array(
+            'newest', 
+            'oldest', 
+            'low_review', 
+            'rating_count_asc',
+            'high_review',
+            'rating_count_desc',
+            'low_rating',
+            'high_rating',
+            'featured',
+            'nearest',
+            'farthest',
+            'random',
+            'az',
+            'high_views',
+            'low_views',
+            'expensive',
+            'cheap'
+        );
+
+        $params['is_featured']   = array(
+            'description'        => __( 'Limit response to featured resouces.' ),
+            'default'            => null,
+            'enum'               => array(0, 1),
+            'validate_callback'  => 'rest_validate_request_arg',
+        );           
 
         $params['sdist']    = array(
             'description'        => __( 'Limit response to resources within some distance from current position.' ),
@@ -92,6 +117,7 @@ class Geodir_REST_Listings_Controller extends WP_REST_Posts_Controller {
             'description'        => __( 'retrieve result sort by some content.' ),
             'type'               => 'string',
             'default'            => $default_sort_by,
+            'enum'               => $sort_by_options,
             'validate_callback'  => 'rest_validate_request_arg',
         );
 
@@ -165,25 +191,25 @@ class Geodir_REST_Listings_Controller extends WP_REST_Posts_Controller {
             'sanitize_callback'  => 'absint',
             'validate_callback'  => 'rest_validate_request_arg',
         );
-        $params['order'] = array(
-            'description'        => __( 'Order sort attribute ascending or descending.' ),
-            'type'               => 'string',
-            'default'            => $default_order,
-            'enum'               => array( 'asc', 'desc' ),
-            'validate_callback'  => 'rest_validate_request_arg',
-        );
-        $params['orderby'] = array(
-            'description'        => __( 'Sort collection by object attribute.' ),
-            'type'               => 'string',
-            'default'            => $default_orderby,
-            'enum'               => $orderby,
-            'validate_callback'  => 'rest_validate_request_arg'
-        );
-        $params['orderby_rendered']    = array(
-            'description'           => __( 'All sorting options for listings.' ),
-            'type'                  => 'array',
-            'enum'                  => $orderby_rendered
-        );
+//        $params['order'] = array(
+//            'description'        => __( 'Order sort attribute ascending or descending.' ),
+//            'type'               => 'string',
+//            'default'            => $default_order,
+//            'enum'               => array( 'asc', 'desc' ),
+//            'validate_callback'  => 'rest_validate_request_arg',
+//        );
+        // $params['orderby'] = array(
+        //     'description'        => __( 'Sort collection by object attribute.' ),
+        //     'type'               => 'string',
+        //     'default'            => $default_orderby,
+        //     'enum'               => $orderby,
+        //     'validate_callback'  => 'rest_validate_request_arg'
+        // );
+        // $params['orderby_rendered']    = array(
+        //     'description'           => __( 'All sorting options for listings.' ),
+        //     'type'                  => 'array',
+        //     'enum'                  => $orderby_rendered
+        // );
 
         $post_type_obj = get_post_type_object( $this->post_type );
         if ( $post_type_obj->hierarchical || 'attachment' === $this->post_type ) {
@@ -231,8 +257,106 @@ class Geodir_REST_Listings_Controller extends WP_REST_Posts_Controller {
         return $params;
     }
 
+    /**
+     * Prepare links for the request.
+     *
+     * @param WP_Post $post Post object.
+     * @return array Links for the given post.
+     */
+    protected function prepare_links( $post ) {
+        $base = sprintf( '/%s/%s', $this->namespace, $this->rest_base );
+
+        // Entity meta
+        $links = array(
+            'self' => array(
+                'href'   => rest_url( trailingslashit( $base ) . $post->ID ),
+            ),
+            'collection' => array(
+                'href'   => rest_url( $base ),
+            ),
+            'about'      => array(
+                'href'   => rest_url( '/wp/v2/types/' . $this->post_type ),
+            ),
+        );
+
+        if ( ( in_array( $post->post_type, array( 'post', 'page' ) ) || post_type_supports( $post->post_type, 'author' ) )
+            && ! empty( $post->post_author ) ) {
+            $links['author'] = array(
+                'href'       => rest_url( '/wp/v2/users/' . $post->post_author ),
+                'embeddable' => true,
+            );
+        };
+
+        if ( in_array( $post->post_type, array( 'post', 'page' ) ) || post_type_supports( $post->post_type, 'comments' ) ) {
+            $replies_url = rest_url( '/wp/v2/comments' );
+            $replies_url = add_query_arg( 'post', $post->ID, $replies_url );
+            $links['replies'] = array(
+                'href'         => $replies_url,
+                'embeddable'   => true,
+            );
+        }
+
+        if ( in_array( $post->post_type, array( 'post', 'page' ) ) || post_type_supports( $post->post_type, 'revisions' ) ) {
+            $links['version-history'] = array(
+                'href' => rest_url( trailingslashit( $base ) . $post->ID . '/revisions' ),
+            );
+        }
+        $post_type_obj = get_post_type_object( $post->post_type );
+        if ( $post_type_obj->hierarchical && ! empty( $post->post_parent ) ) {
+            $links['up'] = array(
+                'href'       => rest_url( trailingslashit( $base ) . (int) $post->post_parent ),
+                'embeddable' => true,
+            );
+        }
+
+        // If we have a featured media, add that.
+        if ( $featured_media = get_post_thumbnail_id( $post->ID ) ) {
+            $image_url = rest_url( 'wp/v2/media/' . $featured_media );
+            $links['https://api.w.org/featuredmedia'] = array(
+                'href'       => $image_url,
+                'embeddable' => true,
+            );
+        }
+        if ( ! in_array( $post->post_type, array( 'attachment', 'nav_menu_item', 'revision' ) ) ) {
+            $attachments_url = rest_url( 'wp/v2/media' );
+            $attachments_url = add_query_arg( 'parent', $post->ID, $attachments_url );
+            $links['https://api.w.org/attachment'] = array(
+                'href'       => $attachments_url,
+            );
+        }
+
+        $taxonomies_controller = new Geodir_REST_Taxonomies_Controller;
+        
+        $taxonomies = get_object_taxonomies( $post->post_type );
+        if ( ! empty( $taxonomies ) ) {
+            $links['https://api.w.org/term'] = array();
+
+            foreach ( $taxonomies as $tax ) {
+                $taxonomy_obj = get_taxonomy( $tax );
+                // Skip taxonomies that are not public.
+                if ( empty( $taxonomy_obj->show_in_rest ) ) {
+                    continue;
+                }
+
+                $tax_base = ! empty( $taxonomy_obj->rest_base ) ? $taxonomy_obj->rest_base : $tax;
+                $terms_url = add_query_arg(
+                    'post',
+                    $post->ID,
+                    rest_url( $this->namespace . '/' . $tax_base )
+                );
+
+                $links['https://api.w.org/term'][] = array(
+                    'href'       => $terms_url,
+                    'taxonomy'   => $tax,
+                    'embeddable' => true,
+                );
+            }
+        }
+
+        return $links;
+    }
     public function get_items( $request ) {
-        global $sdist, $sgeo_lon, $sgeo_lat, $sort_by, $s;
+        global $sdist, $sgeo_lon, $sgeo_lat, $sort_by, $s, $is_featured;
         $args                         = array();
         $args['author__in']           = $request['author'];
         $args['author__not_in']       = $request['author_exclude'];
@@ -248,11 +372,12 @@ class Geodir_REST_Listings_Controller extends WP_REST_Posts_Controller {
         $args['post_parent__in']      = $request['parent'];
         $args['post_parent__not_in']  = $request['parent_exclude'];
         $args['post_status']          = $request['status'];
-        $s                             = $args['s'] = $request['search'];
-        $sdist                         = $args['sdist'] = $request['sdist'];
-        $sgeo_lon                      = $args['sgeo_lon'] = $request['sgeo_lon'];
-        $sgeo_lat                      = $args['sgeo_lat'] = $request['sgeo_lat'];
-        $sort_by                       = $args['sort_by']  = $request['sort_by'];
+        $s                            = $args['s']           = $request['search'];
+        $sdist                        = $args['sdist']       = $request['sdist'];
+        $sgeo_lon                     = $args['sgeo_lon']    = $request['sgeo_lon'];
+        $sgeo_lat                     = $args['sgeo_lat']    = $request['sgeo_lat'];
+        $sort_by                      = $args['sort_by']     = $request['sort_by'];
+        $is_featured                  = $args['is_featured'] = $request['is_featured']; 
 
         $args['date_query'] = array();
         // Set before into date query. Date query must be specified as an array of an array.
@@ -291,14 +416,15 @@ class Geodir_REST_Listings_Controller extends WP_REST_Posts_Controller {
         }
 
         add_action('pre_get_posts', array($this, 'ido_geodir_listing_loop_filter'), 1);
+        
         // add_filter( 'posts_request', array($this,'my_posts_request_filter') );
+        
         $posts_query = new WP_Query();
         $query_result = $posts_query->query( $query_args );
         // var_dump($query_result);
         remove_action('pre_get_posts', array($this, 'ido_geodir_listing_loop_filter'), 1);
 
         $posts = array();
-        add_filter( "rest_prepare_{$this->post_type}",array($this, 'add_distance_to_response'),10,3);
         foreach ( $query_result as $post ) {
             if ( ! $this->check_read_permission( $post ) ) {
                 continue;
@@ -306,7 +432,6 @@ class Geodir_REST_Listings_Controller extends WP_REST_Posts_Controller {
             $data = $this->prepare_item_for_response( $post, $request );
             $posts[] = $this->prepare_response_for_collection( $data );
         }
-        remove_filter( "rest_prepare_{$this->post_type}",array($this, 'add_distance_to_response'),10,3);
 
         $page = (int) $query_args['paged'];
         $total_posts = $posts_query->found_posts;
@@ -394,11 +519,15 @@ class Geodir_REST_Listings_Controller extends WP_REST_Posts_Controller {
 
 
     function ido_searching_filter_where($where) {
-        global $wpdb, $table, $sdist, $sgeo_lat, $sgeo_lon, $s;
+        global $wpdb, $table, $sdist, $sgeo_lat, $sgeo_lon, $s, $is_featured;
 
         $content_where = '';
-        if ($s != '') {
+        if ( $s != '' ) {
             $content_where = "AND ($wpdb->posts.post_content LIKE \"$s\" OR $wpdb->posts.post_content LIKE \"$s%\" OR $wpdb->posts.post_content LIKE \"% $s%\" OR $wpdb->posts.post_content LIKE \"%>$s%\" OR $wpdb->posts.post_content LIKE \"%\n$s%\") ";
+        }
+
+        if ( $is_featured === '0' || $is_featured === '1' ) {
+            $content_where .= "AND " . $table . ".is_featured = " . $is_featured;
         }
             
 
@@ -469,6 +598,12 @@ class Geodir_REST_Listings_Controller extends WP_REST_Posts_Controller {
             case 'low_views':
                 $orderby = "$views_counter_table.count asc ";
                 break;
+            case 'expensive':
+                $orderby = $table . ".geodir_price desc ";
+                break;
+            case 'cheap':
+                $orderby = $table . ".geodir_price asc ";
+                break;
             default:
 
                 break;
@@ -527,6 +662,7 @@ class Geodir_REST_Listings_Controller extends WP_REST_Posts_Controller {
             'sdist',
             'sgeo_lat',
             'sgeo_lon',
+            'is_featured',
         );
         $valid_vars = array_merge( $valid_vars, $rest_valid );
 
@@ -547,16 +683,68 @@ class Geodir_REST_Listings_Controller extends WP_REST_Posts_Controller {
 
         return $valid_vars;
     }
+
     function my_posts_request_filter( $input ) {
         print_r( $input );
         return $input;
     }
 
-    function add_distance_to_response($response, $post, $request){
-        $response->data['distance'] = $post->distance;
-        $response->data['count'] = $post->count;
-        return $response;
+    function create_item( $request ) {
+      // var_dump($request); die();
+      add_action( "rest_insert_{$this->post_type}", array($this, "ido_create_geo_item"), 10, 3);
+      $response = parent::create_item($request);
+      remove_action("rest_insert_{$this->post_type}", array($this, "ido_create_geo_item"), 10, 3);
+      return $response;
     }
 
+    function ido_create_geo_item( $post, $request, $is_create = true ) {
+        global $wpdb, $plugin_prefix;
+
+        $table = $plugin_prefix . $this->post_type . '_detail';
+        
+        $params = $request->get_params();
+
+        $post_id          = $post->ID;
+        $post_title       = $post->post_title;
+        $post_status      = $post->post_status;
+        $post_tags        = isset($params['post_tags'])?$params['post_tags']:'';
+        $gd_advertcategory= isset($params['gd_advertcategory'])?$params['gd_advertcategory']:'';
+        $is_featured      = '0';
+        $featured_image   = isset($params['featured_image'])?$params['featured_image']:'/default.jpg';
+        $submit_time      = time();
+        $submit_ip        = $_SERVER['REMOTE_ADDR'];
+        $post_address     = isset($params['post_address'])?$params['post_zip']:'';
+        $post_city        = 'Calgary';
+        $post_region      = 'Region';
+        $post_country     = 'Canada';
+        $post_zip         = isset($params['post_zip'])?$params['post_zip']:'';
+        $post_latitude    = isset($params['post_latitude'])?$params['post_latitude']:'';
+        $post_longitude   = isset($params['post_longitude'])?$params['post_longitude']:'';
+        $post_mapview     = isset($params['post_mapview'])?$params['post_mapview']:'ROADMAP';
+        $geodir_contact   = isset($params['geodir_contact'])?$params['geodir_contact']:'';
+        $geodir_email     = isset($params['geodir_email'])?$params['geodir_email']:'';
+        $geodir_price     = isset($params['geodir_price'])?(int)$params['geodir_price']:0;
+
+        $wpdb->query(
+            $wpdb->prepare( 
+                "INSERT INTO " . $table . "(`post_id`, `post_title`, `post_status`, `post_tags`, `gd_advertcategory`, `is_featured`, `featured_image`, `submit_time`, `submit_ip`, `post_address`, `post_city`,`post_region`, `post_country`, `post_zip`, `post_latitude`, `post_longitude`, `post_mapview`, `geodir_contact`, `geodir_email`, `geodir_price`)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %d)", array($post_id, $post_title, $post_status, $post_tags, $gd_advertcategory, $is_featured, $featured_image, $submit_time,$submit_ip, $post_address, $post_city, $post_region, $post_country, $post_zip, $post_latitude, $post_longitude, $post_mapview, $geodir_contact, $geodir_email, $geodir_price)
+            )
+        );
+
+        if ( isset( $params['post_image_ids'] ) ) {
+            $post_image_ids         = $params['post_image_ids'];
+            $post_images_ids_array  = explode ( ',' , $post_image_ids );
+
+            $table = 'wp_geodir_attachments';
+            foreach ($post_images_ids_array as $post_images_id) {
+                $wpdb->query(
+                    $wpdb->prepare( 
+                        "UPDATE " . $table . " SET post_id = %d WHERE ID = %d", array($post_id, $post_images_id)
+                    )
+                );
+            }
+        }
+    } 
 
 }
